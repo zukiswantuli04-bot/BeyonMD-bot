6652907b8ff09a095975672c086cc7628195dd05
mruniquehacker <vaibhavkumawat1003+knightbot@gmail.com>
2025-04-23 19:51:33 +0530

    First commit

--- commands/quote.js
+++ commands/quote.js
@@ -2,12 +2,17 @@ const axios = require('axios');
 
 module.exports = async function (sock, chatId) {
     try {
-        const response = await axios.get('https://type.fit/api/quotes');
-        const quotes = response.data;
-        const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-        await sock.sendMessage(chatId, { text: `${randomQuote.text} - ${randomQuote.author || 'Unknown'}` });
+        const response = await axios.get('https://zenquotes.io/api/random', {
+            httpsAgent: new (require('https').Agent)({ rejectUnauthorized: false })
+        });
+        const quote = response.data[0];
+        await sock.sendMessage(chatId, { 
+            text: `"${quote.q}"\n- ${quote.a}`
+        });
     } catch (error) {
         console.error('Error fetching quote:', error);
-        await sock.sendMessage(chatId, { text: 'Sorry, I could not fetch a quote right now.' });
+        await sock.sendMessage(chatId, { 
+            text: 'Sorry, I could not fetch a quote right now.'
+        });
     }
 };
