f36e7b14bf662597137d3b7b98af34a22d0c40d2
mruniquehacker <vaibhavkumawat1003+knightbot@gmail.com>
2025-06-11 16:07:48 +0530

    First commit

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ const settings = {
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "2.0.2",
+  version: "2.0.3",
 };
 
 module.exports = settings;
